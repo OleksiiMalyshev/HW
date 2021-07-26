7de8530b5eaeeb8c332e0a83ce6e696d08715400
Oleksii Malyshev <kyrsan.kaifat@gmail.com>
2021-07-26 21:26:28 +0300

    Adding the hw#11 with unit tests and doctests

--- hw_testing_1/calc.py
+++ hw_testing_1/calc.py
@@ -1,4 +1,6 @@
-import doctest
+class RootError(Exception):
+    pass
+
 
 class Calc:
     @staticmethod
@@ -13,30 +15,90 @@ class Calc:
         """
         return a + b
 
-
-
     @staticmethod
     def minus(a, b):
+        """Віднімання чисел
+        >>> Calc.minus(10,9)
+        1
+
+        Від числа a віднімається число b
+        :param a: перше число
+        :param b: друге число
+        :return: результат a - b
+        """
         return a - b
 
     @staticmethod
     def mul(a, b):
+        """Множення чисел
+        >>> Calc.mul(2,4)
+        8
+
+        Число a множиться на число b
+        :param a: перше число
+        :param b: друге число
+        :return: результат a * b
+        """
         return a * b
 
     @staticmethod
     def div(a, b):
+        """Ділення чисел
+        >>> Calc.div(10, 5)
+        2
+
+        Число a ділиться на число b
+        :param a: перше число
+        :param b: друге число
+        :return: результат a / b
+        """
         return a / b
 
     @staticmethod
     def percent(a, b):
-        return (b*100) / a
+        """Відсоток від числа
+        >>> Calc.percent(100, 10)
+        10.0
+
+        :param a: основне число (100%)
+        :param b: друге число (X%)
+        :return: Скільки відсотків число b від числа a
+        """
+        return (b * 100) / a
 
     @staticmethod
     def power(a, b):
-        return a**b
+        """Піднесення в степінь
+        >>> Calc.power(10, 2)
+        100
+
+
+        :param a: число - основа
+        :param b: число - степінь
+        :return: Число a ** b
+        """
+        return a ** b
 
     @staticmethod
     def root(a, b):
-        if a < 0 :
+        """Корінь з числа a за основою b
+        >>> Calc.root(25, 2)
+        5
 
-# doctest.testmod()
+        Для плюсових чисел використовується формула
+        round(pow(a, float(1 / b)))
+        Для від'ємних чисел та з основою кратною 3 формула
+        round(-pow(abs(a), float(1 / b)))
+        :param a: число під коренем
+        :param b: основа кореня
+        :return: Результат кореня
+        """
+        if a > 0:
+            return round(pow(a, float(1 / b)))
+        elif a < 0:
+            if b % 2 == 0:
+                raise RootError
+            else:
+                return round(-pow(abs(a), float(1 / b)))
+        else:
+            return 0
--- hw_testing_1/unit_tests.py
+++ hw_testing_1/unit_tests.py
@@ -10,13 +10,35 @@ class TestCalc(TestCase):
     def test_sum(self):
         self.assertEqual(Calc.plus(1, 2), 3)
         self.assertEqual(Calc.plus(2, 2), 4)
-        self.assertEqual(Calc.plus(5, 4), 10)
+        self.assertNotEqual(Calc.plus(5, 4), 10)
 
     def test_minus(self):
-        pass
+        self.assertEqual(Calc.minus(10, 5), 5)
+        self.assertEqual(Calc.minus(100, 2), 98)
+        self.assertNotEqual(Calc.minus(10, 1), 10)
 
     def test_mul(self):
-        pass
+        self.assertEqual(Calc.mul(5, 5), 25)
+        self.assertEqual(Calc.mul(11, 11), 121)
+        self.assertNotEqual(Calc.mul(5, 5), 10)
 
     def test_div(self):
-        pass
+        self.assertEqual(Calc.div(10, 2), 5)
+        with self.assertRaises(ZeroDivisionError):
+            Calc.div(100, 0)  # Помилка ділення на 0
+        self.assertNotEqual(Calc.div(10, 2), 1)
+
+    def test_percent(self):
+        self.assertEqual(Calc.percent(100, 10), 10)
+        self.assertNotEqual(Calc.percent(100, 10), 1)
+
+    def test_power(self):
+        self.assertEqual(Calc.power(2, 4), 16)
+        self.assertEqual(Calc.power(2, 5), 32)
+        self.assertNotEqual(Calc.power(2, 5), 16)
+
+    def test_root(self):
+        self.assertEqual(Calc.root(25, 2), 5)
+        with self.assertRaises(ZeroDivisionError):
+            self.assertEqual(Calc.root(10, 0), 10)  # помилка
+        self.assertNotEqual(Calc.root(100, 5), 10)
